9464d0d7b863a8c02ac2b0a4a83a61471dd099da
SleepyPandas <tommyrobotics1@gmail.com>
2026-01-28 21:02:22 -0500

    Refactor: Changed Function signature to be more abstract, accepts any GPIO pin and Port

--- Core/Inc/DHT11_Driver.h
+++ Core/Inc/DHT11_Driver.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
-int DHT11_Read_Data(int *temp, int *hum);
+int DHT11_Read_Data(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, int *temp,
+                    int *hum);
 
 #endif /* DHT11_DRIVER_H */
